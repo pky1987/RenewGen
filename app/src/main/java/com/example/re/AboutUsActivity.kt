60e076faea834d51a1229386a1e9907ceec37b2b
pky1987 <py430896@gmail.com>
2025-11-07 06:13:51 +0530

    fix name correctly

--- app/src/main/java/com/example/re/AboutUsActivity.kt
+++ app/src/main/java/com/example/re/AboutUsActivity.kt
@@ -14,7 +14,7 @@ class AboutUsActivity : AppCompatActivity() {
     private lateinit var textView: TextView
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: AboutUsAdapter
-    private val texts = listOf("Satyam Singh", "Dheeraj Yadav", "Mr. Omkar Madyal")
+    private val texts = listOf("Sachin Tiwari", "Prakash Yadav")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
